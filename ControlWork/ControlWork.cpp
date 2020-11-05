cb6581af5c1398a15f37f8eca5192d42f45f32fc
Andrey582 <gnataly2106@mail.ru>
2020-11-05 13:08:39 +0300

    ControlWork

---
+++ ControlWork/ControlWork.cpp
@@ -0,0 +1,15 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cstdio>
+#include <ctime>
+int main()
+{
+	struct tm* data;
+	char Now_data[20];
+	const time_t t = time(NULL);
+	data = localtime(&t);
+	strftime(Now_data, 20, "%d.%m.%Y", data);
+	printf("%s\n", Now_data);
+	system("pause");
+	return 0;
+}
